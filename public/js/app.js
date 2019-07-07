dc0606f1c74628e9de23c6c8b96ff0badac4b6de
Robin Tabard <rpctab@gmail.com>
2019-07-07 22:42:38 +0200

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+const weatherForm = document.querySelector('form');
+const locationInput = document.querySelector('#weather');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e.target)
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+    fetch(`http://localhost:3000/weather?address=${locationInput.value}`).then((response) => {
+        response.json().then(({
+            error,
+            location,
+            forecast
+        }) => {
+            if (error) {
+                locationInput.value = '';
+                return messageOne.textContent = error;
+            }
+            messageOne.textContent = location;
+            messageTwo.textContent = forecast;
+            locationInput.value = '';
+        })
+    })
+})
